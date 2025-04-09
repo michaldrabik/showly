2fd4ebfb1b4513b201f36c36419f0579aeb64655
michaldrabik <michal.drabik0@gmail.com>
2025-04-09 09:33:57 +0200

    Updates and refactors

--- ui-episodes/src/main/java/com/michaldrabik/ui_episodes/details/links/EpisodeLinksBottomSheet.kt
+++ ui-episodes/src/main/java/com/michaldrabik/ui_episodes/details/links/EpisodeLinksBottomSheet.kt
@@ -2,22 +2,21 @@ package com.michaldrabik.ui_episodes.details.links
 
 import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.os.Parcelable
 import android.view.View
+import androidx.core.net.toUri
 import androidx.core.os.bundleOf
 import com.michaldrabik.ui_base.BaseBottomSheetFragment
 import com.michaldrabik.ui_base.utilities.extensions.onClick
 import com.michaldrabik.ui_base.utilities.extensions.openWebUrl
 import com.michaldrabik.ui_base.utilities.extensions.requireParcelable
 import com.michaldrabik.ui_base.utilities.viewBinding
-import com.michaldrabik.ui_model.Ids
-import com.michaldrabik.ui_model.Episode
-import com.michaldrabik.ui_navigation.java.NavigationArgs
 import com.michaldrabik.ui_episodes.R
 import com.michaldrabik.ui_episodes.databinding.ViewEpisodeLinksBinding
-import com.michaldrabik.ui_model.IdTmdb
+import com.michaldrabik.ui_model.Episode
+import com.michaldrabik.ui_model.Ids
+import com.michaldrabik.ui_navigation.java.NavigationArgs
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.parcelize.Parcelize
 
@@ -26,26 +25,32 @@ class EpisodeLinksBottomSheet : BaseBottomSheetFragment(R.layout.view_episode_li
 
   @Parcelize
   data class Options(
-    val showIdTmdb: IdTmdb,
+    val showIds: Ids,
     val ids: Ids,
-    val title: String,
     val season: Int,
     val episodeNumber: Int,
   ) : Parcelable
 
   companion object {
-    fun createBundle(showIdTmdb: IdTmdb, episode: Episode): Bundle {
-      val options = Options(showIdTmdb, episode.ids, episode.title, episode.season, episode.number)
-      return bundleOf(NavigationArgs.ARG_EPISODE_LINK to options)
-      }
+    fun createBundle(
+      showIds: Ids,
+      episode: Episode,
+    ): Bundle {
+      val options = Options(
+        showIds = showIds,
+        ids = episode.ids,
+        season = episode.season,
+        episodeNumber = episode.number,
+      )
+      return bundleOf(NavigationArgs.ARG_OPTIONS to options)
+    }
   }
 
   private val binding by viewBinding(ViewEpisodeLinksBinding::bind)
 
-  private val options by lazy { requireParcelable<Options>(NavigationArgs.ARG_EPISODE_LINK) }
-  private val showIdTmdb by lazy { options.showIdTmdb.id }
+  private val options by lazy { requireParcelable<Options>(NavigationArgs.ARG_OPTIONS) }
+  private val showIds by lazy { options.showIds }
   private val ids by lazy { options.ids }
-  private val title by lazy { options.title }
   private val season by lazy { options.season }
   private val episodeNumber by lazy { options.episodeNumber }
 
@@ -60,18 +65,36 @@ class EpisodeLinksBottomSheet : BaseBottomSheetFragment(R.layout.view_episode_li
   }
 
   private fun setupView() {
-    val query = "$title season $season episode $episodeNumber"
     with(binding) {
+      val searchQuery = "${showIds.slug.id} season $season episode $episodeNumber"
       viewEpisodeLinksGoogle.onClick {
-        openWebUrl("https://www.google.com/search?q=$query")
+        openWebUrl("https://www.google.com/search?q=$searchQuery")
       }
+
       viewEpisodeLinksDuckDuck.onClick {
-        openWebUrl("https://duckduckgo.com/?q=$query")
+        openWebUrl("https://duckduckgo.com/?q=$searchQuery")
       }
-      viewEpisodeLinksButtonClose.onClick { closeSheet() }
+
+      viewEpisodeLinksButtonClose.onClick {
+        closeSheet()
+      }
     }
+
+    setTraktLink()
     setTmdbLink()
+    setTvdbLink()
     setImdbLink()
+  }
+
+  private fun setTraktLink() {
+    binding.viewEpisodeLinksTrakt.run {
+      if (ids.trakt.id == -1L) {
+        alpha = 0.5F
+        isEnabled = false
+      } else {
+        onClick { openWebUrl("https://trakt.tv/shows/${showIds.trakt.id}/seasons/$season/episodes/$episodeNumber") }
+      }
+    }
   }
 
   private fun setTmdbLink() {
@@ -81,7 +104,20 @@ class EpisodeLinksBottomSheet : BaseBottomSheetFragment(R.layout.view_episode_li
         isEnabled = false
       } else {
         onClick {
-          openWebUrl("https://www.themoviedb.org/tv/${showIdTmdb}/season/${season}/episode/${episodeNumber}")
+          openWebUrl("https://www.themoviedb.org/tv/${showIds.tmdb.id}/season/$season/episode/$episodeNumber")
+        }
+      }
+    }
+  }
+
+  private fun setTvdbLink() {
+    binding.viewEpisodeLinksTvdb.run {
+      if (ids.tvdb.id == -1L) {
+        alpha = 0.5F
+        isEnabled = false
+      } else {
+        onClick {
+          openWebUrl("https://thetvdb.com/series/${showIds.tvdb.id}/episodes/${ids.tvdb.id}")
         }
       }
     }
@@ -95,7 +131,7 @@ class EpisodeLinksBottomSheet : BaseBottomSheetFragment(R.layout.view_episode_li
       } else {
         onClick {
           val i = Intent(Intent.ACTION_VIEW)
-          i.data = Uri.parse("imdb:///title/${ids.imdb.id}")
+          i.data = "imdb:///title/${ids.imdb.id}".toUri()
           try {
             startActivity(i)
           } catch (e: ActivityNotFoundException) {
